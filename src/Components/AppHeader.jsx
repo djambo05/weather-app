10316e05f222cd0717e2480ae5080bea58538af7
Djamalutin <fisher.gerryy@mail.ru>
2023-05-25 14:17:48 +0300

    feat: starting project

---
+++ src/Components/AppHeader.jsx
@@ -0,0 +1,14 @@
+export const AppHeader = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        backgroundColor: "#443f3f",
+        width: "100vw",
+        height: "80px",
+      }}
+    ></div>
+  );
+};
